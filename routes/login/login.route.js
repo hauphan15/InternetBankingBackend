01fbcd144d571bfbaecda451f5d79a916a0aa925
hauphan15 <49446469+hauphan15@users.noreply.github.com>
2020-12-31 22:51:54 +0700

    .

--- routes/login/login.route.js
+++ routes/login/login.route.js
@@ -20,25 +20,10 @@ router.post('/', async(req, res) => {
     const userId = result.ID;
     const accessToken = generateAccessToken(userId);
 
-    const checkingAccountInfo = await checkingAccountModel.getByID(userId);
-    checkingAccountInfo[0].DateCreate = moment(checkingAccountInfo[0].DateCreate).format('DD-MM-YYYY');
-    let savingAccountInfo = await savingAccountModel.getByUserID(userId);
-
-
-    if (savingAccountInfo.length === 0) {
-        savingAccountInfo = [{ AccountNumber: '' }]
-    } else {
-        for (let index = 0; index < savingAccountInfo.length; index++) {
-            savingAccountInfo[index].DateCreate = moment(savingAccountInfo[index].DateCreate).format('DD-MM-YYYY');
-        }
-    }
-
     return res.json({
         authenticated: true,
         accessToken: accessToken,
         userInfo: result,
-        checkingAccountInfo: checkingAccountInfo[0],
-        savingAccountInfo: savingAccountInfo,
         message: 'Login successfully'
     })
 })
